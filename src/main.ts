7f3fd9aa9ba8a78114c731b0a3e6ad2a042a1d5a
samolchik <samolchik22@gmail.com>
2023-08-08 12:00:55 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function start() {
+  const PORT = process.env.PORT || 3001;
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
+}
+start().then();
